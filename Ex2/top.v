2489cc3bee6e5fdd78a9de2eec1067fdd19c8089
Cloud User <centos@ip-172-31-5-161.eu-west-2.compute.internal>
2020-06-09 09:28:18 +0000

    solution to ex2

--- Ex2/top.v
+++ Ex2/top.v
@@ -22,11 +22,28 @@
 
 module mux(
     //Todo: define inputs here
-    );
-    
-    //Todo: define registers and wires here
+    input a, b, sel,
+    output out
+);
 
+    //Todo: define registers and wires here
+reg clk_reg;
+reg out_reg;
+    
+
+parameter t = 5;
+initial begin
+	clk_reg = 0;
+	forever
+        #t clk_reg=!clk_reg;
+end
+
+assign out = out_reg;
 
     //Todo: define your logic here                 
+always @ (posedge clk_reg) begin
+	out_reg = (sel && b) || (!sel && a);
+end
+	
       
 endmodule
