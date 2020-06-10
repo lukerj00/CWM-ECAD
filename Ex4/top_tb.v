34d9a772763c37228f23fc7d198ca51dbbc1ec61
Cloud User <centos@ip-172-31-5-161.eu-west-2.compute.internal>
2020-06-10 11:18:49 +0000

    work in progress

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,59 @@
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb(
+   );
+   
+   parameter CLK_PERIOD = 10;
+   reg clk;
+   reg reset;
+   reg button;
+   reg [2:0] throw;
+   //output wire [2:0] throw_out;
+
+   initial begin
+   clk = 1'b0;
+   forever
+     #(CLK_PERIOD/2) clk=~clk;
+   end
+
+  initial begin
+  reset = 1;
+  button = 0;
+  throw  = 3'b000;
+
+  #(CLK_PERIOD*5)
+  if (throw!=3'b001)
+   begin
+    $display("***TEST FAILED! reset=1, throw=%d", throw);
+   err = 1;
+   end
+
+  reset=0;
+  #(CLK_PERIOD*5)
+  if (throw=3'b001)
+   begin
+    $display("***TEST FAILED! reset=0, throw=%d", throw);
+  err = 1;
+  end
+
+  button = 1;
+  #(CLK_PERIOD*5)
+
+end
+
+ counter top (
+ .clk (clk)
+ .reset (reset)
+ .button (button)
+ .throw (throw)
+);
+
+intial begin
+
+
+
+
 
